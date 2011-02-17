ad4564a71617e768b098874bfb0d165189cec9fe
Josh Ourisman <josh@joshourisman.com>
2011-02-17 16:40:00 -0500

    basic outline of main concept

---
+++ shortly.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+def expand(short_url):
+    return 'http://www.google.com/'
+
+@app.route("/<short_url>+")
+def to_info_page(short_url):
+    return render_template('template.html', short_url=short_url)
+
+@app.route("/<short_url>")
+def to_long_url(short_url):
+    return redirect(expand(short_url))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0')
+
