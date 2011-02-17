7bdbb76d1a7cbbef34c8801bedb3f4efb217f98a
Josh Ourisman <josh@joshourisman.com>
2011-02-17 16:53:32 -0500

    using new model in app

--- shortly/__init__.py
+++ shortly/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, redirect
 
-app = Flask(__name__)
+from models import Url
 
-def expand(short_url):
-    return 'http://www.google.com/'
+app = Flask(__name__)
 
 @app.route("/<short_url>+")
 def to_info_page(short_url):
-    return render_template('template.html', short_url=short_url)
+    url = Url(short_url)
+    return render_template('template.html', short_url=url.short_url)
 
 @app.route("/<short_url>")
 def to_long_url(short_url):
-    return redirect(expand(short_url))
+    url = Url(short_url)
+    return redirect(url.long_url)
 
