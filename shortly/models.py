75842d60ffee2fd7dd4efe91ee88534a7f7f06cd
Josh Ourisman <josh@joshourisman.com>
2011-02-17 18:12:04 -0500

    better field names and storing more information

--- shortly/models.py
+++ shortly/models.py
@@ -1,12 +1,14 @@
 from redis import Redis
 
+import datetime
+
 class Url(object):
     def __init__(self, short_url=None):
         self.r = Redis(db=0)
         
         if short_url is not None:
             self.s = short_url
-            self.l = self.r.get('url:%s:id' % self.s)
+            self.l = self.r.get('url:%s:long_url' % self.s)
 
     @property
     def short_url(self):
@@ -21,6 +23,9 @@ class Url(object):
             url_hash = '%x' % self.r.incr('next.url.id')
         else:
             url_hash = short_url
-        self.r.set('url:%s:id' % url_hash, long_url)
+            
+        self.r.set('url:%s:long_url' % url_hash, long_url)
+        self.r.set('url:%s:created' % url_hash, datetime.datetime.now())
+        self.r.set('url:%s:short_url'% long_url, url_hash)
         self.r.push('global:urls', url_hash)
         return url_hash
